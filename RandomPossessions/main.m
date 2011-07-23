b67014c591fb01b13847be35d6fcfd6632cd7824
Xiaochun Shen <snowdonshen@gmail.com>
2011-07-23 22:22:33 +0800

    init Array and Display it

--- RandomPossessions/main.m
+++ RandomPossessions/main.m
@@ -12,9 +12,33 @@ int main (int argc, const char * argv[])
 {
 
     @autoreleasepool {
+    
+        // Create a mutable array, store its address in items variable
+        NSMutableArray *items = [[NSMutableArray alloc] init];
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        // Send the message addObject: to the NSMutableArray pointed to 
+        // by the variable items, passing a string each time.
+        [items addObject:@"One"];
+        [items addObject:@"Two"];
+        [items addObject:@"Three"];
+        
+        // Send another message, insertObject:atIndex, to that same array object
+        [items insertObject:@"Zero" atIndex:0];
+        
+        // For every item in the array as determined by sending count to the items array
+        for (int i = 0; i < [items count]; i++) {
+            //We get the ith object from the arra and pass it as an argument to NSLog,
+            // which implicitly sends the description message to that object
+            NSLog(@"%@", [items objectAtIndex:i]);
+        }
+        
+        
+        // Release the array
+        [items release];
+        
+        // Don't leave items pointing at freed memory!
+        items = nil;
+        
         
     }
     return 0;
